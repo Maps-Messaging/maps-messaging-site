8efe1123e4c06a35b2f94ec753bed6afa72bf389
krital <alex@kritikal.org>
2025-01-28 07:10:34 +0200

    Preparing for github pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -34,7 +34,7 @@ const config: Config = {
    // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Maps-Messaging', // Usually your GitHub org/user name
-  projectName: 'maps-messaging-site.github.io', // Usually your repo name
+  projectName: 'maps-messaging-site', // Usually your repo name
   trailingSlash: false,
   deploymentBranch: 'gh-pages', 
 
